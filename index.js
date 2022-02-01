c581995cb39634e99ff732adbd06a34843446a84
Christian Volz <christianbvolz@gmail.com>
2022-02-01 18:19:18 -0300

    requisito 2

--- index.js
+++ index.js
@@ -17,8 +17,22 @@ app.get('/', (_request, response) => {
 app.get('/talker', rescue(async (_req, res) => {
   const talkers = await fs.readFile('./talker.json', 'utf-8')
     .then((result) => JSON.parse(result));
+
   if (!talkers) return res.status(HTTP_OK_STATUS).json([]);
+
   res.status(HTTP_OK_STATUS).json(talkers);
+}));
+
+app.get('/talker/:id', rescue(async (req, res) => {
+  const { id } = req.params;
+  const talkers = await fs.readFile('./talker.json', 'utf-8')
+    .then((result) => JSON.parse(result));
+
+  const talker = talkers.find((tal) => tal.id === +id);
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(200).json(talker);
 }));
 
 app.listen(PORT, () => {
